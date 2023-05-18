11cac5b005ae137db2425d6c0862863e49bdeae7
bimol009 <110928529+bimol009@users.noreply.github.com>
2023-05-18 11:25:04 +0600

    tailwind,daisyUi

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../Shared/Navbar/Navbar';
+import Footer from './../Shared/Footer/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Footer/>
+        </div>
+    );
+};
+
+export default Main;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,13 +1,21 @@
 
 import App from './../App';
 import { createBrowserRouter } from 'react-router-dom';
+import Main from '../Layout/Main';
+import Home from '../Pages/Home/Home';
 
 const router = createBrowserRouter([
 
 
   {
     path:'/',
-    element:<App></App>
+    element:<Main></Main>,
+    children:[
+      {
+        path:'/',
+        element:<Home></Home>
+      }
+    ]
   }
  
   
